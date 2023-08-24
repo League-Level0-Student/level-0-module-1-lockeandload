a11433fa2c9e8aecb518abb4c58f3b0acb531255
jereycui2008 <jeremycuid@gmail.com>
2023-08-23 19:54:36 -0700

    and i'm done

--- src/_03_if_else/_1_unbirthday/Unbirthday.java
+++ src/_03_if_else/_1_unbirthday/Unbirthday.java
@@ -1,5 +1,23 @@
+
 package _03_if_else._1_unbirthday;
 
+import javax.swing.JOptionPane;
+
 public class Unbirthday {
-	String input =  JOptionpane.input;
+	public static void main(String[] args) {
+	String unbirthday = JOptionPane.showInputDialog("when is your birthday master cheff? .exe 01/01");
+if (unbirthday.equals("08/23")) {
+    
+    String unbirthday1 = JOptionPane.showInputDialog("go play some halo your amazing : )");
+
 }
+
+if (!(unbirthday.equals("08/23"))) {
+    
+String unbirthday3 = JOptionPane.showInputDialog("get on fortnite you lozer!");
+
+}
+
+}
+}
+	
