3473fc2d3bf5a8afd75023318ae24b9a9a845dbc
jereycui2008 <jeremycuid@gmail.com>
2023-08-30 19:55:08 -0700

    e

--- src/_03_if_else/_2_remarkable/Remarkable.java
+++ src/_03_if_else/_2_remarkable/Remarkable.java
@@ -8,11 +8,23 @@ public class Remarkable {
 public static void main(String[] args) {
 
 		// 1. Save something remarkable about each person in a variable.
-
+	String Remarkable = JOptionPane.showInputDialog("what is your name first letter cap.");
 		// 2. Ask the user to enter a name. Store their answer in a variable.
-
+	if (Remarkable.equals("Alexander")) {
+	    
+	    String coolFacts = JOptionPane.showInputDialog("you own a galaxy watch 4");
 		// 3. In a pop-up, tell the user what is remarkable about that person. 
+	}
+	else if (Remarkable.equals("Ariana")) {
+		    
+		    String coolFact = JOptionPane.showInputDialog("your uncle works for apple");
+	    }
+	else {
+		String Remark = JOptionPane.showInputDialog("I don'k know if you spelt it rong or not but your cring");
+	}
 
+		
+	
 	}
 }
 
