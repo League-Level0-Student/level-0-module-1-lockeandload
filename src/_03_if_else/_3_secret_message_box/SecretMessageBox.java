5bc534c9e976c682c50e37c6dc277af416ae918d
league <league@imac-16.attlocal.net>
2023-10-20 16:59:40 -0700

    more copy paste and ya I can see this

--- src/_03_if_else/_3_secret_message_box/SecretMessageBox.java
+++ src/_03_if_else/_3_secret_message_box/SecretMessageBox.java
@@ -27,8 +27,8 @@ public class SecretMessageBox {
 
 		// 2. Using a pop-up, ask the first person for a secret message and store it in a variable
 		String secret = JOptionPane.showInputDialog("???");
-		
-		// 3. Now use a pop-up to tell the NEXT user that they can only see the secret message 
+
+		// 3. Now use a pop-up to tell the NEXT user that they can only see the secret message
 		//    if they can guess the passcode
 		String guess = JOptionPane.showInputDialog("guss the password");
 		// 4. If their guess matches the password, show them the secret message
--- src/_04_int/_1_riddler/TheRiddler.java
+++ src/_04_int/_1_riddler/TheRiddler.java
@@ -2,18 +2,124 @@ package _04_int._1_riddler;
 //    Copyright (c) The League of Amazing Programmers 2013-2019
 //    Level 0
 
-
+import javax.swing.JOptionPane;
 
 public class TheRiddler {
 
 	public static void main(String[] args) {
-
+//
+		//jokes
+		//"What do you call a factory that makes okay products?" "A satisfactory."
+		//"What did the janitor say when he jumped out of the closet?" "Supplies!"
+		//"Have you heard about the chocolate record player? It sounds pretty sweet."
+		//"What did the ocean say to the beach?" "Nothing, it just waved."
+		//"Why do seagulls fly over the ocean?" "Because if they flew over the bay, we'd call them bagels."
+		//"I only know 25 letters of the alphabet. I don't know y."
+		//"How does the moon cut his hair?" "Eclipse it."
+		//"What did one wall say to the other?" "I'll meet you at the corner."
+		//"What did the zero say to the eight?" "That belt looks good on you."
+		//"Where do fruits go on vacation?" "Pear-is!"
+		//"I asked my dog what's two minus two. He said nothing."
+		//"What did Baby Corn say to Mama Corn?" "Where's Pop Corn?"
+		//"What's the best thing about Switzerland?" "I don't know, but the flag is a big plus."
+		//"What does a sprinter eat before a race?" "Nothing, they fast!"
+		//"Where do you learn to make a banana split?" "Sundae school."
+		//"What has more letters than the alphabet?" "The post office!"
+		//"Dad, did you get a haircut?" "No, I got them all cut!"
+		//"What do you call a poor Santa Claus?" "St. Nickel-less."		
 		// 1. Make a variable to hold the score
 
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
-
+		String gg1 = JOptionPane.showInputDialog("What do you call a factory that makes okay products?");
+		if (gg1.equals ("he")) {		
+			JOptionPane.showMessageDialog(null, "correct");
+		}
+		//
+		String gg2 = JOptionPane.showInputDialog("What did the janitor say when he jumped out of the closet?")
+				;if (gg2.equals ("he")) {		
+			JOptionPane.showMessageDialog(null, "correct");
+		}
+		//
+		String gg3 = JOptionPane.showInputDialog("What did the ocean say to the beach?");
+		if (gg3.equals ("he")) {		
+			JOptionPane.showMessageDialog(null, "correct");
+		}
+		//
+		String gg4 = JOptionPane.showInputDialog("Why do seagulls fly over the ocean?");
+		if (gg4.equals ("he")) {		
+			JOptionPane.showMessageDialog(null, "correct");
+		}
+		//
+		String gg5 = JOptionPane.showInputDialog("I only know 25 letters of the alphabet.");
+		if (gg5.equals ("he")) {		
+			JOptionPane.showMessageDialog(null, "correct");
+		}
+		//
+		String gg6 = JOptionPane.showInputDialog("How does the moon cut his hair?");
+		if (gg6.equals ("he")) {		
+			JOptionPane.showMessageDialog(null, "correct");
+		}
+		//
+		String gg7 = JOptionPane.showInputDialog("What did one wall say to the other?");
+		if (gg7.equals ("he")) {		
+			JOptionPane.showMessageDialog(null, "correct");
+		}
+		//
+		String gg8 = JOptionPane.showInputDialog("What did the zero say to the eight?");
+		if (gg8.equals ("he")) {		
+			JOptionPane.showMessageDialog(null, "correct");
+		}
+		//
+		String gg9 = JOptionPane.showInputDialog("Where do fruits go on vacation?");
+		if (gg9.equals ("he")) {		
+			JOptionPane.showMessageDialog(null, "correct");
+		}
+		//
+		String gg10 = JOptionPane.showInputDialog("I asked my dog what's two minus two.");
+		if (gg10.equals ("he")) {		
+			JOptionPane.showMessageDialog(null, "correct");
+		}
+		//
+		String gg11 = JOptionPane.showInputDialog("What did Baby Corn say to Mama Corn?");
+		if (gg11.equals ("he")) {		
+			JOptionPane.showMessageDialog(null, "correct");
+		}
+		//
+		String gg12 = JOptionPane.showInputDialog("What's the best thing about Switzerland?");
+		if (gg12.equals ("he")) {		
+			JOptionPane.showMessageDialog(null, "correct");
+		}
+		//
+		String gg13 = JOptionPane.showInputDialog("What does a sprinter eat before a race?");
+		if (gg13.equals ("he")) {		
+			JOptionPane.showMessageDialog(null, "correct");
+		}
+		//
+		String gg14 = JOptionPane.showInputDialog("Where do you learn to make a banana split?")
+				;if (gg14.equals ("he")) {		
+			JOptionPane.showMessageDialog(null, "correct");
+		}
+		////
+		String gg15 = JOptionPane.showInputDialog("What has more letters than the alphabet?");
+		if (gg15.equals ("he")) {		
+			JOptionPane.showMessageDialog(null, "correct");
+		}
+		//
+		String gg16 = JOptionPane.showInputDialog("Dad, did you get a haircut?");
+		if (gg16.equals ("he")) {		
+			JOptionPane.showMessageDialog(null, "correct");
+		}
+		//
+		String gg17 = JOptionPane.showInputDialog("What do you call a poor Santa Claus?");
+		if (gg17.equals ("he")) {		
+			JOptionPane.showMessageDialog(null, "correct");
+		}
+		//
+		String gg18 = JOptionPane.showInputDialog("");
 		// 4. If they got the answer right, pop up "correct!" and increase the score by one
-
+		if (gg18.equals ("he")) {		
+			JOptionPane.showMessageDialog(null, "correct");
+		}
 		// 5. Otherwise, say "wrong" and tell them the answer
 
 		// 6. Add some more riddles
