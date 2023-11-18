4e3aafebda54a73dea0edf05d8cc429add7c17b5
league <league@imac-22.attlocal.net>
2023-11-17 17:01:09 -0800

    ya?!?!

--- src/_05_for_loops/_1_my_ages/MyAges.java
+++ src/_05_for_loops/_1_my_ages/MyAges.java
@@ -1,6 +1,7 @@
 package _05_for_loops._1_my_ages;
 
 public class MyAges {
+	
 	public static void main(String[] args) {
 		
 		/** This program will use loops to print all the ages you have been alive, 
@@ -8,19 +9,22 @@ public class MyAges {
 
 		// Make an int variable called age, to track how many times the loop has repeated
 		//        Start its value at zero.
-		
+		int age = 0;
 		// Change the value of yourCurrentAge to match your age
-		int yourCurrentAge = 0;
+		int yourCurrentAge = 99;
 				
 		
 		//  WHILE LOOP. Start a while loop to repeat while age <= yourCurrentAge
-				
-			//  Print the age variable
-		
-			//  Add one to the age variable
-		
-		//  End the while loop here
+ while (age <= 99) {
 
+
+ 
+			//  Print the age variable
+ System.out.println (age);
+			//  Add one to the age variable
+age += 1;
+		//  End the while loop here
+ }
 		//  Run the program. Did it print all your ages from 0 to your current age?
 		
 		/*** Now you are going to do the same thing, but this time use a for loop. 
@@ -30,11 +34,11 @@ public class MyAges {
 		
 		//  FOR LOOP. Start a for loop to repeat while age <= your age, like this:
 		//  for ( age=0; age <= yourAge; age +=1 ) {
-		
+ for ( age=0; age <= yourCurrentAge; age +=1 ) {
 			//  Print the age variable
-		
+	 System.out.println (age);
 		//  End the for loop here
-		
+ }
 		//  Run the program again. 		
 		//  Check that both loops printed the same numbers to the console
 	}
---
+++ src/_05_for_loops/_2_badgers/Badger_Mushroom_Snenk.java
@@ -0,0 +1,17 @@
+package _05_for_loops._2_badgers;
+
+public class Badger_Mushroom_Snenk {
+	public static void main(String[] args) {
+	for ( ) {
+}
+	
+	}
+	
+}
+//Badger, Badger, Badger, Badger, Badger, Badger, Badger,Badger, Badger,Badger, Badger, Badger
+//Mushroom, Mushroom
+
+//Badger, Badger, Badger,Badger, Badger, Badger, Badger,Badger, Badger, Badger, Badger, Badger
+//Mushroom, Mushroom
+
+//A Snake!!!
